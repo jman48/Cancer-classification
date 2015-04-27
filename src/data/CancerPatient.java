9ea028fe56b824c8099d321367f369220e807413
John Armstrong <j.armstrong484@gmail.com>
2015-04-27 13:47:04 +1200

    Refactored the data class to read in all information. Data file was changed to use -1 instead of ?.

--- src/data/CancerPatient.java
+++ src/data/CancerPatient.java
@@ -1,206 +1,287 @@
 package data;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * A class to hold information about a cancer patient.
+ * 
+ * @author John Armstrong
+ *
+ */
 public class CancerPatient {
 
+    private int id;	//Sample ID
+    private int ct; // Clump thickness
+    private int usz; // Uniformity of Cell Size
+    private int ushp; // Uniformity of Cell Shape
+    private int ma; // Marginal Adhesion
+    private int sesz; // SingleEpithelial Cell Size
+    private int bn; // Bare Nuclei
+    private int bc; // Bland Chromatin
+    private int nn; // Normal Nucleoli
+    private int m; // Mitoses
+    private int cancerClass; // Cancer class - 2 for benign, 4 for malignant
+
+    public static final int BENIGN = 2;
+    public static final int MALIGNANT = 4;
+
+    private CancerPatient(PatientBuilder builder) {
+	this.id = builder.id;
+	this.ct = builder.ct;
+	this.usz = builder.usz;
+	this.ushp = builder.ushp;
+	this.ma = builder.ma;
+	this.sesz = builder.sesz;
+	this.bn = builder.bn;
+	this.bc = builder.bc;
+	this.nn = builder.nn;
+	this.m = builder.m;
+	this.cancerClass = builder.cancerClass;
+    }
+
+    /**
+     * Sample ID
+     * @return
+     */
+    public int getId() {
+	return id;
+    }
+
+    /**
+     * Get Clump thickness
+     * 
+     * @return
+     */
+    public int getCt() {
+	return ct;
+    }
+
+    /**
+     * Get Uniformity of Cell Size
+     * 
+     * @return
+     */
+    public int getUsz() {
+	return usz;
+    }
+
+    /**
+     * Get Uniformity of Cell Shape
+     * 
+     * @return
+     */
+    public int getUshp() {
+	return ushp;
+    }
+
+    /**
+     * Get Marginal Adhesion
+     * 
+     * @return
+     */
+    public int getMa() {
+	return ma;
+    }
+
+    /**
+     * Get SingleEpithelial Cell Size
+     * 
+     * @return
+     */
+    public int getSesz() {
+	return ma;
+    }
+
+    /**
+     * Get Bare Nuclei
+     * 
+     * @return
+     */
+    public int getBn() {
+	return bn;
+    }
+
+    /**
+     * Get Bland Chromatin
+     * 
+     * @return
+     */
+    public int getBc() {
+	return bc;
+    }
+
+    /**
+     * Get Normal Nucleoli
+     * 
+     * @return
+     */
+    public int getNn() {
+	return nn;
+    }
+
+    /**
+     * Get Mitoses
+     * 
+     * @return
+     */
+    public int getM() {
+	return m;
+    }
+
+    /**
+     * Cancer Class - 2 for benign, 4 for malignant
+     * 
+     * @return
+     */
+    public int getCancerClass() {
+	return cancerClass;
+    }
+
+    @Override
+    public String toString() {
+	return "Patient - id: " + id + " ct: " + ct + " usz: " + usz
+		+ " ushp: " + ushp + " ma: " + ma + " bn: " + bn + " bc: " + bc
+		+ " nn: " + nn + " m: " + m;
+    }
+
+    public static class PatientBuilder {
 	private int id;
 	private int ct; // Clump thickness
 	private int usz; // Uniformity of Cell Size
 	private int ushp; // Uniformity of Cell Shape
 	private int ma; // Marginal Adhesion
+	private int sesz; // SingleEpithelial Cell Size
 	private int bn; // Bare Nuclei
 	private int bc; // Bland Chromatin
 	private int nn; // Normal Nucleoli
 	private int m; // Mitoses
+	private int cancerClass; // Class - 2 for benign, 4 for malignant
 
 	/**
-	 * Construct a new cancer patient
-	 * 
-	 * @param id
-	 *            The id of the cancer patient
-	 * @param ct
-	 *            Clump thickness
-	 * @param usz
-	 *            Uniformity of Cell Size
-	 * @param ushp
-	 *            Uniformity of Cell Shape
-	 * @param ma
-	 *            Marginal Adhesion
-	 * @param bn
-	 *            Bare Nuclei
-	 * @param bc
-	 *            Bland Chromatin
-	 * @param nn
-	 *            Normal Nucleoli
-	 * @param m
-	 *            Mitoses
+	 * Construct a new Cancer patient builder.
+	 * @param id The sample id
 	 */
-	public CancerPatient(int id, int ct, int usz, int ushp, int ma, int bn,
-			int bc, int nn, int m) {
-		this.id = id;
-		this.ct = ct;
-		this.usz = usz;
-		this.ushp = ushp;
-		this.ma = ma;
-		this.bn = bn;
-		this.bc = bc;
-		this.nn = nn;
-		this.m = m;
-	}
-	
-	private CancerPatient(PatientBuilder builder) {
-		this.id = builder.id;
-		this.ct = builder.ct;
-		this.usz = builder.usz;
-		this.ushp = builder.ushp;
-		this.ma = builder.ma;
-		this.bn = builder.bn;
-		this.bc = builder.bc;
-		this.nn = builder.nn;
-		this.m = builder.m;
-	}
-
-	public int getId() {
-		return id;
+	public PatientBuilder(int id) {
+	    this.id = id;
 	}
 
 	/**
-	 * Get Clump thickness
+	 * Clump thickness
 	 * 
+	 * @param ct Clump thickness
 	 * @return
 	 */
-	public int getCt() {
-		return ct;
+	public PatientBuilder ct(int ct) {
+	    this.ct = ct;
+	    return this;
 	}
 
 	/**
-	 * Get Uniformity of Cell Size
+	 * Uniformity of Cell Size
 	 * 
+	 * @param usz Uniformity of Cell Size
 	 * @return
 	 */
-	public int getUsz() {
-		return usz;
+	public PatientBuilder usz(int usz) {
+	    this.usz = usz;
+	    return this;
 	}
 
 	/**
-	 * Get Uniformity of Cell Shape
+	 * Uniformity of Cell Shape
 	 * 
+	 * @param ushp Uniformity of Cell Shape
 	 * @return
 	 */
-	public int getUshp() {
-		return ushp;
+	public PatientBuilder ushp(int ushp) {
+	    this.ushp = ushp;
+	    return this;
 	}
 
 	/**
-	 * Get Marginal Adhesion
+	 * Marginal Adhesion
 	 * 
+	 * @param ma Marginal Adhesion
 	 * @return
 	 */
-	public int getMa() {
-		return ma;
+	public PatientBuilder ma(int ma) {
+	    this.ma = ma;
+	    return this;
 	}
 
 	/**
-	 * Get Bare Nuclei
+	 * SingleEpithelial Cell Size
 	 * 
+	 * @param sesz SingleEpithelial Cell Size
 	 * @return
 	 */
-	public int getBn() {
-		return bn;
+	public PatientBuilder sesz(int sesz) {
+	    this.sesz = sesz;
+	    return this;
 	}
 
 	/**
-	 * Get Bland Chromatin
+	 * Bare Nuclei
 	 * 
+	 * @param bn Bare Nuclei
 	 * @return
 	 */
-	public int getBc() {
-		return bc;
+	public PatientBuilder bn(int bn) {
+	    this.bn = bn;
+	    return this;
 	}
 
 	/**
-	 * Get Normal Nucleoli
+	 * Bland Chromatin
 	 * 
+	 * @param bc Bland Chromatin
 	 * @return
 	 */
-	public int getNn() {
-		return nn;
+	public PatientBuilder bc(int bc) {
+	    this.bc = bc;
+	    return this;
 	}
 
 	/**
-	 * Get Mitoses
+	 * Normal Nucleoli
 	 * 
+	 * @param nn Normal Nucleoli
 	 * @return
 	 */
-	public int getM() {
-		return m;
-	}
-	
-	@Override
-	public String toString() {
-		return "Patient - id: " + id + " ct: " + ct + " usz: " + usz + " ushp: " + ushp + " ma: " + ma + " bn: " + bn + " bc: " + bc + " nn: " + nn + " m: " + m; 		
+	public PatientBuilder nn(int nn) {
+	    this.nn = nn;
+	    return this;
 	}
 
-	public static class PatientBuilder {
-		private int id;
-		private int ct; // Clump thickness
-		private int usz; // Uniformity of Cell Size
-		private int ushp; // Uniformity of Cell Shape
-		private int ma; // Marginal Adhesion
-		private int bn; // Bare Nuclei
-		private int bc; // Bland Chromatin
-		private int nn; // Normal Nucleoli
-		private int m; // Mitoses
-
-		public PatientBuilder(int id) {
-			this.id = id;
-		}
-
-		public PatientBuilder ct(int ct) {
-			this.ct = ct;
-			return this;
-		}
-
-		public PatientBuilder usz(int usz) {
-			this.usz = usz;
-			return this;
-		}
-
-		public PatientBuilder ushp(int ushp) {
-			this.ushp = ushp;
-			return this;
-		}
-
-		public PatientBuilder ma(int ma) {
-			this.ma = ma;
-			return this;
-		}
-
-		public PatientBuilder bn(int bn) {
-			this.bn = bn;
-			return this;
-		}
-
-		public PatientBuilder bc(int bc) {
-			this.bc = bc;
-			return this;
-		}
-
-		public PatientBuilder nn(int nn) {
-			this.nn = nn;
-			return this;
-		}
-
-		public PatientBuilder m(int m) {
-			this.m = m;
-			return this;
-		}
-		
-		public CancerPatient build() {
-			return new CancerPatient(this);
-		}
+	/**
+	 * Mitoses
+	 * 
+	 * @param m Mitoses
+	 * @return
+	 */
+	public PatientBuilder m(int m) {
+	    this.m = m;
+	    return this;
 	}
+
+	/**
+	 * Cancer class. 2 for benign, 4 for malignant
+	 * 
+	 * @param cancerClass Cancer Class. 2 for benign, 4 for malignant
+	 * @return
+	 */
+	public PatientBuilder cancerClass(int cancerClass) {
+	    this.cancerClass = cancerClass;
+	    return this;
+	}
+
+	/**
+	 * Return a new Cancer Patient based on the values set on this builder
+	 * object
+	 * 
+	 * @return A new Cancer Patient
+	 */
+	public CancerPatient build() {
+	    return new CancerPatient(this);
+	}
+    }
 
 }
--- src/data/Data.java
+++ src/data/Data.java
@@ -8,59 +8,55 @@ import java.util.Scanner;
 
 import data.CancerPatient.PatientBuilder;
 
+/**
+ * A class to read in data and store it.
+ * 
+ * @author John Armstrong
+ *
+ */
 public class Data {
-	// The default location for the data file
-	public static final String defaultFileLocation = "data/breast-cancer-wisconsin.data";
+    // The default location for the data file
+    public static final String defaultFileLocation = "data/breast-cancer-wisconsin.data";
 
-	// Our collection of cancer patients from the data file
-	private List<CancerPatient> patients = new ArrayList<CancerPatient>();
+    // Our collection of cancer patients from the data file
+    private List<CancerPatient> patients = new ArrayList<CancerPatient>();
 
-	/**
-	 * Construct a new Data object. This data object will automatically read all
-	 * the data in from the file specified or will use the default file location
-	 * 
-	 * @param fileName
-	 *            - The name of the file holding all the data
-	 * @throws FileNotFoundException
-	 */
-	public Data(String fileName) throws FileNotFoundException {
-		if (fileName == null || fileName.isEmpty()) {
-			readFile(defaultFileLocation);
-		} else {
-			readFile(fileName);
-		}
+    /**
+     * Construct a new Data object. This data object will automatically read all
+     * the data in from the file specified or will use the default file location
+     * 
+     * @param fileName
+     *            - The name of the file holding all the data
+     * @throws FileNotFoundException
+     */
+    public Data(String fileName) throws FileNotFoundException {
+	if (fileName == null || fileName.isEmpty()) {
+	    readFile(defaultFileLocation);
+	} else {
+	    readFile(fileName);
 	}
+    }
 
-	private void readFile(String fileName) throws FileNotFoundException {
-		Scanner fileInput = new Scanner(new File(fileName));
-		
-		while (fileInput.hasNextLine()) {
-			Scanner line = new Scanner(fileInput.nextLine());
-			line.useDelimiter(",");
+    private void readFile(String fileName) throws FileNotFoundException {
+	Scanner fileInput = new Scanner(new File(fileName));
+	
+	while (fileInput.hasNextLine()) {
+	    Scanner line = new Scanner(fileInput.nextLine());
+	    line.useDelimiter(",");
 
-			//Create a new patient builder with an id from the data file
-			PatientBuilder patientBuilder = new PatientBuilder(line.nextInt());
-			List<Integer> attributes = new ArrayList<Integer>();
+	    // Create a new patient builder with an id from the data file
+	    PatientBuilder patientBuilder = new PatientBuilder(line.nextInt())
+		    .ct(line.nextInt()).usz(line.nextInt())
+		    .ushp(line.nextInt()).ma(line.nextInt())
+		    .sesz(line.nextInt())
+		    .bn(line.nextInt()).bc(line.nextInt())
+		    .nn(line.nextInt()).m(line.nextInt()).cancerClass(line.nextInt());
 
-			while (line.hasNext()) {
-				if (line.hasNextInt()) {
-					attributes.add(line.nextInt());
-				} else {
-					// Use -1 if we do not know the attribute
-					attributes.add(-1);
-					line.next();
-				}
-			}
-			line.close();
+	    line.close();
 
-			patientBuilder.ct(attributes.get(0)).usz(attributes.get(1))
-					.ushp(attributes.get(2)).ma(attributes.get(3))
-					.bn(attributes.get(4)).bc(attributes.get(5))
-					.nn(attributes.get(6)).m(attributes.get(7));
-			
-			patients.add(patientBuilder.build());
-			System.out.println(patients.size());
-		}
-		fileInput.close();
+	    patients.add(patientBuilder.build());
 	}
+	fileInput.close();
+	System.out.println("Read " + patients.size() + " cancer patients information");
+    }
 }
